a2a98508da15e7f58787c314af1be68c4a0def0b
byronnDev <mikel.e.g.2004@gmail.com>
2023-12-13 13:32:21 +0100

    test3 passed

--- src/dia2/ejercicio3.test.js
+++ src/dia2/ejercicio3.test.js
@@ -3,3 +3,14 @@ const isOddNumber = (num) => {
 };
 
 //Añade los test
+describe("isOddNumber", () => {
+  it("should return true when is odd number and false when is even", () => {
+    const result = isOddNumber(1);
+
+    expect(result).toBeFalsy()
+
+    const result2 = isOddNumber(2);
+
+    expect(result2).toBeTruthy()
+  });
+})
