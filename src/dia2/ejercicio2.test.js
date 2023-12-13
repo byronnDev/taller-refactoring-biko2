41fbb10eb0d5798f5df5c0e16392576e2f5418f8
byronnDev <mikel.e.g.2004@gmail.com>
2023-12-13 13:41:42 +0100

    refactor test 2

--- src/dia2/ejercicio2.test.js
+++ src/dia2/ejercicio2.test.js
@@ -2,14 +2,17 @@ const isPositiveNumber = (num) => {
   return num >= 0
 };
 
-describe("sum", () => {
-  it("should return true when is poistive number and false when is negative", () => {
-    const result = isPositiveNumber(1);
+describe("isPositiveNumber", () => {
+  it("should return true when is poistive number", () => {
+    const positiveNumber = isPositiveNumber(1);
 
-    expect(result).toBeTruthy()
-
-    const result2 = isPositiveNumber(-5);
-
-    expect(result2).toBeFalsy()
+    expect(positiveNumber).toBeTruthy();
   });
+
+  it("should return false when is negative", () => {
+    const negativeNumber = isPositiveNumber(-5);
+
+    expect(negativeNumber).toBeFalsy();
+  });
+
 });
