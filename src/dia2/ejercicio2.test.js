7639aa76296eeb3f781e0ca710352b7c316f4018
byronnDev <mikel.e.g.2004@gmail.com>
2023-12-13 13:31:37 +0100

    test2 passed and refactoresd

--- src/dia2/ejercicio2.test.js
+++ src/dia2/ejercicio2.test.js
@@ -1,17 +1,15 @@
 const isPositiveNumber = (num) => {
-  if(num> = 0) return true
-
-  return false
+  return num >= 0
 };
 
 describe("sum", () => {
   it("should return true when is poistive number and false when is negative", () => {
     const result = isPositiveNumber(1);
-    
+
     expect(result).toBeTruthy()
-    
+
     const result2 = isPositiveNumber(-5);
-    
+
     expect(result2).toBeFalsy()
   });
 });
